ce8a9d93188302cdb176ec6fe7cbad911c9884f3
GiLik154 <118507239+GiLik154@users.noreply.github.com>
2022-12-13 23:04:09 +0900

    Update BoardList.java

--- board2/src/boardfunction/BoardList.java
+++ board2/src/boardfunction/BoardList.java
@@ -5,23 +5,22 @@ import java.io.OutputStreamWriter;
 import java.util.List;
 
 public class BoardList {
-	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public void output(List<BoardInfo> list) {
-		try {
-			for (BoardInfo i : list) { // 향상된 for문
-				BoardInfo boardInfo = (BoardInfo) i; // 형변환
-				bw.write("글 번호 : " + boardInfo.num); // 출력
-				bw.write(" || 글 제목 : " + boardInfo.title);
-				bw.write(" || 글 작성자 : " + boardInfo.writer);
-				bw.write(" || 글 조회수 : " + boardInfo.hits);
-				bw.write(" || 글 작성일 : " + boardInfo.date + "\n");
-			}
-			bw.write("================================================================================================="
-					+ "\n");
-			bw.flush();
-		} catch (Exception e) { // 예외처리
-			e.printStackTrace();
-		}
-	}
+    public void output(List<BoardInfo> list) {
+        try {
+            for (BoardInfo i : list) { // 향상된 for문
+                bw.write("글 번호 : " + i.num); // 출력
+                bw.write(" || 글 제목 : " + i.title);
+                bw.write(" || 글 작성자 : " + i.writer);
+                bw.write(" || 글 조회수 : " + i.hits);
+                bw.write(" || 글 작성일 : " + i.date + "\n");
+            }
+            bw.write("================================================================================================="
+                    + "\n");
+            bw.flush();
+        } catch (Exception e) { // 예외처리
+            e.printStackTrace();
+        }
+    }
 }
